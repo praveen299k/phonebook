b3313dd641f145e26be28c1e54a6381b58bdd43f
praveen299k <praveen.299k@gmail.com>
2025-12-25 16:17:32 +0530

    add search functionality by name

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const [nametoSearch, setNameToSearch] = useState("");
+
+  const personsToShow =
+    nametoSearch.trim("") === ""
+      ? persons
+      : persons.filter((person) => {
+          return person.name
+            .toLowerCase()
+            .includes(nametoSearch.toLowerCase().trim(""));
+        });
+
+  const handleFilterPhonebook = (e) => {
+    setNameToSearch(e.target.value);
+  };
 
   const handleNameChagne = (e) => {
     setNewName(e.target.value);
@@ -32,9 +50,19 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter phonebook with{" "}
+        <input
+          type="text"
+          value={nametoSearch}
+          onChange={handleFilterPhonebook}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name:{" "}
@@ -48,8 +76,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div>
+      {personsToShow.map((person) => (
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
       ))}
