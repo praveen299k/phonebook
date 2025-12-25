3f1dd35b595decb7cdf7343e4978e39082076706
praveen299k <praveen.299k@gmail.com>
2025-12-25 15:30:21 +0530

    feat: add basic phonebook functionality

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChagne = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const person = { name: newName };
+    setPersons(persons.concat(person));
+    setNewName("");
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNameChagne} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
